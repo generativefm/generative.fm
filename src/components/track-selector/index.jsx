f84063694290c6072a433f20323224673b6538d5
metalex9 <alexbainter@gmail.com>
2018-10-18 22:33:34 -0500

    Start reworking track selection styles

--- src/components/track-selector/index.jsx
+++ src/components/track-selector/index.jsx
@@ -6,7 +6,6 @@ import './styles.scss';
 const TrackSelector = () => (
   <div className="track-selector">
     <h1 className="title">Generative Music</h1>
-    <h2 className="subtitle">Select a piece</h2>
     <ul className="track-list">
       {pieces.map((piece, i) => (
         <li className="track-list__track" key={i}>
@@ -14,10 +13,8 @@ const TrackSelector = () => (
         </li>
       ))}
     </ul>
-    <h2 className="subtitle">What is generative music?</h2>
-    <div className="description">
-      Generative music is a term popularized by Brian Eno to describe music that
-      is ever-different and changing, and that is created by a system.
+    <div className="alex-bainter-link">
+      <a href="https://alexbainter.com">made by alex bainter</a>
     </div>
   </div>
 );
