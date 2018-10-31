ff3c49e733aac0ef25a5533f9b640b8039af6d72
metalex9 <alexbainter@gmail.com>
2018-10-30 23:46:04 -0500

    Persist state to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/root.reducer';
 import piecesMiddleware from './middleware/pieces.middleware';
+import localStorageMiddleware from './middleware/local-storage.middleware';
+import STATE_STORAGE_KEY from './middleware/local-storage.middleware/key';
 
-const store = createStore(rootReducer, applyMiddleware(piecesMiddleware));
+const storedStateJSON = window.localStorage.getItem(STATE_STORAGE_KEY);
+const storedState =
+  typeof storedStateJSON === 'undefined' ? {} : JSON.parse(storedStateJSON);
+
+const initialState = Object.assign({}, storedState, { isPlaying: false });
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(piecesMiddleware, localStorageMiddleware)
+);
 
 export default store;
