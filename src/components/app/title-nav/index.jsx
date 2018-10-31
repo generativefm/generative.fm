34e13db0ade207a076595baa221fcec9d75ddbef
metalex9 <alexbainter@gmail.com>
2018-10-31 00:15:01 -0500

    Restyle title nav

--- src/components/app/title-nav/index.jsx
+++ src/components/app/title-nav/index.jsx
@@ -6,7 +6,20 @@ import './title-nav.scss';
 const TitleNavComponent = ({ activeTabId, onTabClick }) => {
   return (
     <div className="title-nav">
-      <h1 className="title-nav__main-title">Generative Music - Alex Bainter</h1>
+      <h1 className="title-nav__title title-nav__title--primary">
+        Generative Music
+      </h1>
+      <h2 className="title-nav__title title-nav__title--secondary">
+        By{' '}
+        <a
+          className="title-nav__title__secret-link"
+          href="https://alexbainter.com"
+          //eslint-disable-next-line react/jsx-no-target-blank
+          target="_blank"
+        >
+          Alex Bainter
+        </a>
+      </h2>
       <ul className="title-nav__tab-list">
         {Reflect.ownKeys(tabs).map(tabId => (
           <li key={tabId} className="title-nav__tab-list__item">
