a91f620ca74d752d931cf291b888283372c6cbc8
metalex9 <alexbainter@gmail.com>
2019-02-16 23:02:12 -0600

    Update changelog

--- src/store/middleware/pieces.middleware/play-piece.js
+++ src/store/middleware/pieces.middleware/play-piece.js
@@ -20,21 +20,22 @@ const playPiece = (piece, getState) => {
     piece.volumeNode = pieceVol;
 
     // METERING
-    const meter = new Tone.Meter();
-    pieceVol.connect(meter);
-    let maxDb = -Infinity;
-    const updateMaxDb = () => {
-      const db = meter.getValue();
-      if (db > maxDb) {
-        maxDb = db;
-      }
-    };
-    setInterval(() => {
-      updateMaxDb();
-    }, 0);
-    setInterval(() => {
-      console.log(maxDb);
-    }, 1000);
+    // most pieces between 0.1 and 0.3
+    // const meter = new Tone.Meter();
+    // pieceVol.connect(meter);
+    // let maxDb = -Infinity;
+    // const updateMaxDb = () => {
+    //   const db = meter.getValue();
+    //   if (db > maxDb) {
+    //     maxDb = db;
+    //   }
+    // };
+    // setInterval(() => {
+    //   updateMaxDb();
+    // }, 0);
+    // setInterval(() => {
+    //   console.log(maxDb);
+    // }, 1000);
 
     piece.makePiece(pieceVol, noop).then(cleanUp => {
       piece.cleanUp = cleanUp;
