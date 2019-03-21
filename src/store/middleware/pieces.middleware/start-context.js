3c8229df9ad50748f666700b303cca3973891f91
metalex9 <alexbainter@gmail.com>
2019-03-20 23:05:08 -0500

    Fix mp3 compatibility checking before playing silent sound

--- src/store/middleware/pieces.middleware/start-context.js
+++ src/store/middleware/pieces.middleware/start-context.js
@@ -1,10 +1,8 @@
 import { context } from 'tone';
-import sampleFormat from '../../../config/sample-format';
 
 const SILENT_SOUND_URL =
   'data:audio/mp3;base64,//MkxAAHiAICWABElBeKPL/RANb2w+yiT1g/gTok//lP/W/l3h8QO/OCdCqCW2Cw//MkxAQHkAIWUAhEmAQXWUOFW2dxPu//9mr60ElY5sseQ+xxesmHKtZr7bsqqX2L//MkxAgFwAYiQAhEAC2hq22d3///9FTV6tA36JdgBJoOGgc+7qvqej5Zu7/7uI9l//MkxBQHAAYi8AhEAO193vt9KGOq+6qcT7hhfN5FTInmwk8RkqKImTM55pRQHQSq//MkxBsGkgoIAABHhTACIJLf99nVI///yuW1uBqWfEu7CgNPWGpUadBmZ////4sL//MkxCMHMAH9iABEmAsKioqKigsLCwtVTEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVV//MkxCkECAUYCAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV';
-
-let shouldPlaySilence = sampleFormat === 'mp3';
+let shouldPlaySilence = true;
 
 // "inspired by" (read: ripped off) https://github.com/tambien/StartAudioContext/blob/master/StartAudioContext.js
 const startAudioContext = () => {
@@ -24,12 +22,14 @@ const startAudioContext = () => {
     if (shouldPlaySilence) {
       shouldPlaySilence = false;
       // Play a silent sound via an HTML audio element to enable proper audio playback in iOS
-      const tag = document.createElement('audio');
-      tag.controls = false;
-      tag.preload = 'auto';
-      tag.loop = false;
-      tag.src = SILENT_SOUND_URL;
-      tag.play();
+      const audio = document.createElement('audio');
+      if (audio.canPlayType('audio/mp3') !== '') {
+        audio.controls = false;
+        audio.preload = 'auto';
+        audio.loop = false;
+        audio.src = SILENT_SOUND_URL;
+        audio.play();
+      }
     }
   }
 };
