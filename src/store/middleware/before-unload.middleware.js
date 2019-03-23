0c7afa378eaba80567205c73fe0ae2b6b4bce6f1
metalex9 <alexbainter@gmail.com>
2019-03-23 15:29:46 -0500

    Fix beforeunload listener

--- src/store/middleware/before-unload.middleware.js
+++ src/store/middleware/before-unload.middleware.js
@@ -3,22 +3,29 @@ import isProduction from '@config/is-production';
 const handleBeforeUnload = event => {
   event.preventDefault();
   event.returnValue = '';
+  return '';
 };
 
 const beforeUnloadMiddleware = store => next => {
   let isListening = false;
-  return action => {
-    const result = next(action);
-    const { isPlaying } = store.getState();
-    if (isProduction && isPlaying && !isListening) {
-      isListening = true;
-      window.addEventListener('beforeunload', handleBeforeUnload);
-    } else if (isListening) {
-      isListening = false;
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    }
-    return result;
-  };
+  if (isProduction) {
+    return action => {
+      const result = next(action);
+      const { isPlaying } = store.getState();
+      if (isPlaying) {
+        if (!isListening) {
+          isListening = true;
+          window.addEventListener('beforeunload', handleBeforeUnload);
+        }
+      } else if (isListening) {
+        isListening = false;
+        window.removeEventListener('beforeunload', handleBeforeUnload);
+      }
+
+      return result;
+    };
+  }
+  return action => next(action);
 };
 
 export default beforeUnloadMiddleware;
