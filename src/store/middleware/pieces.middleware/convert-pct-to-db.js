1f3b79db9807310529e96ba3add903510533d615
metalex9 <alexbainter@gmail.com>
2019-06-19 11:33:44 -0500

    Make volume slider non-linear

--- src/store/middleware/pieces.middleware/convert-pct-to-db.js
+++ src/store/middleware/pieces.middleware/convert-pct-to-db.js
@@ -1,7 +1,8 @@
 const MAX_VOLUME_DB = 0;
 const MIN_VOLUME_DB = -50;
+const PCT_EXPONENT = 0.5;
 
 const convertPctToDb = pct =>
-  (pct / 100) * (MAX_VOLUME_DB - MIN_VOLUME_DB) + MIN_VOLUME_DB;
+  (pct / 100) ** PCT_EXPONENT * (MAX_VOLUME_DB - MIN_VOLUME_DB) + MIN_VOLUME_DB;
 
 export default convertPctToDb;
