a522bd9c77433abcddc6fb66603c26c49990e925
metalex9 <alexbainter@gmail.com>
2019-08-26 07:33:12 -0500

    Disable Patreon notification

--- src/notifications/index.js
+++ src/notifications/index.js
@@ -1,12 +1,12 @@
 const patreon = {
   message: 'Consider supporting Generative.fm on Patreon',
   link: 'https://www.patreon.com/bePatron?u=2484731',
-  getIsTriggered: state =>
-    Reflect.ownKeys(state.playTime).reduce(
-      (totalPlayTimeMS, pieceId) => totalPlayTimeMS + state.playTime[pieceId],
-      0
-    ) >=
-    10 * 60 * 60 * 1000,
+  getIsTriggered: (/*state*/) => false,
+  // Reflect.ownKeys(state.playTime).reduce(
+  //   (totalPlayTimeMS, pieceId) => totalPlayTimeMS + state.playTime[pieceId],
+  //   0
+  // ) >=
+  // 10 * 60 * 60 * 1000,
 };
 
 export default { patreon };
