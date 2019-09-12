df2c56d24b7ded45621baa3f3e7c44562a3405e1
metalex9 <alexbainter@gmail.com>
2019-09-12 14:42:22 -0500

    Only collect state in production

--- src/store/middleware/save-state-beacon.middleware.js
+++ src/store/middleware/save-state-beacon.middleware.js
@@ -1,9 +1,10 @@
 import API_ENDPOINT from '@config/api-endpoint';
+import isProduction from '@config/is-production';
 import stringify from './stringify-state';
 import isOnline from '../get-online-status';
 
 const saveStateBeaconMiddleware = store => next => {
-  if (navigator.sendBeacon) {
+  if (navigator.sendBeacon && isProduction) {
     window.addEventListener('beforeunload', () => {
       if (isOnline()) {
         navigator.sendBeacon(
