d1bfb2282278f545e29d5c191a406bbba8720a4d
metalex9 <alexbainter@gmail.com>
2020-03-07 19:21:18 -0600

    Remove samples from webpack-offline-plugin cache

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,23 +1,7 @@
 'use strict';
 
 const OfflinePlugin = require('offline-plugin');
-const getSamplesByFormat = require('@generative-music/samples-alex-bainter');
 const configPromise = require('./webpack.config');
-
-const samplesByFormat = getSamplesByFormat('//samples.alexbainter.com');
-
-const allSampleFilenames = ['ogg', 'mp3'].reduce((filenames, format) => {
-  const formatSamples = samplesByFormat[format];
-  return filenames.concat(
-    Object.keys(formatSamples).reduce((sampleFilenames, sampleName) => {
-      const samples = formatSamples[sampleName];
-      if (Array.isArray(samples)) {
-        return sampleFilenames.concat(samples);
-      }
-      return sampleFilenames.concat(Object.values(samples));
-    }, [])
-  );
-}, []);
 
 const getConfig = configPromise.then(config => {
   config.mode = 'production';
@@ -29,14 +13,10 @@ const getConfig = configPromise.then(config => {
         'favicon.ico',
         'manifest.json',
         'https://platform.twitter.com/widgets.js',
-      ].concat(allSampleFilenames),
+      ],
       autoUpdate: true,
       ServiceWorker: {
         events: true,
-      },
-      caches: {
-        main: [':rest:'],
-        optional: allSampleFilenames,
       },
     })
   );
